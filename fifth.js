168601bc4b4804875ace080d1248d380dfbb09af
Chuksspain <chuksspain@gmail.com>
2022-06-01 09:18:39 +0100

    change

---
+++ fifth.js
@@ -0,0 +1,7 @@
+// DON'T TOUCH THIS LINE! (please)
+const word = "skateboard"; //Don't change this line!
+
+// YOUR CODE BELOW THIS LINE:
+word.slice(4);
+word.replace("board", "beard");
+let facialHair = "beard"
